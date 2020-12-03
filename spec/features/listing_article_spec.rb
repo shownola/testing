8f176fa77d5b17d1d4df84ae8c4a3288a6991e26
shownola <swasieko@gmail.com>
2020-12-02 18:06:12 -0700

    Implement new test for displaying message when no articles found

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature 'Listing Articles' do
     @article1 = Article.create(title: 'The first article', body: 'Lorem ipsum dolar sit amet')
     @article2 = Article.create(title: 'The second article', body: "Bacon ipsum ain't that bacon good.")
   end
+
   scenario 'A user lists all articles' do
     visit "/"
 
@@ -14,5 +15,21 @@ RSpec.feature 'Listing Articles' do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+  end
+
+  scenario 'A user has no articles' do
+    Article.delete_all
+    visit "/"
+
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+
+    within("h1#no-articles") do
+      expect(page).to have_content('No Articles Created')
+    end
   end
 end
